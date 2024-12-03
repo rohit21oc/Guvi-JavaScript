03891128036b4833369ef07ae155618c2d8601cf
rohit21oc <rohitkumar15031@gmail.com>
2024-12-03 12:19:34 +0530

    First commit

---
+++ beginner/first.js
@@ -0,0 +1,11 @@
+console.log("Hello Guvi Project!");
+
+console.error("Heloooo");
+console.warn("don't do again");
+
+let name = "Rohit"
+console.log(name);
+
+let age; //Declare the variable
+age = 21; //initialize the variable
+console.log(age);
---
+++ beginner/number.js
@@ -0,0 +1,47 @@
+var age = 21;
+var pi = 3.14;
+var mobileNumber = 9178596235;
+var bigInt = 687685456465;
+var num = 0;
+var negative = -10;
+
+console.log(age)
+console.log(pi)
+console.log(mobileNumber)
+console.log(bigInt)
+
+console.log(typeof age)
+console.log(typeof pi)
+console.log(typeof mobileNumber)
+console.log(typeof bigInt)
+console.log(typeof num)
+console.log(typeof negative)
+
+console.log("type of Infinity: "+typeof Infinity);
+console.log("type of Infinity: "+typeof NaN);
+
+// toString();
+
+let x = 20;
+console.log(x)
+console.log( typeof x.toString() + " = "+ x.toString());
+console.log((65 + 50).toString());
+console.log((65 * 50).toString());
+console.log("50"*3);
+
+var z = 4.5645554;
+console.log(z.toFixed(2));
+
+console.log(z.toPrecision(4));
+ 
+let age1 = '45';
+console.log(age1);
+console.log(Number(age1));
+console.log(typeof age1);
+console.log(typeof Number(age1));
+
+console.log(parseInt("3684878"));
+console.log(parseFloat("3454654.245"));
+
+
+
