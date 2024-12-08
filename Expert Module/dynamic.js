da6ed3a2265d33a105e40d356dc98380889c04f0
rohit21oc <rohitkumar15031@gmail.com>
2024-12-08 11:35:46 +0530

    Expert module

---
+++ Expert Module/dynamic.js
@@ -0,0 +1,13 @@
+h1 = document.querySelector("h1");
+h1.innerHTML = "Hello developer";
+
+customDiv = document.querySelector(".customDiv");
+const h2 = document.createElement("h2");
+console.log(h2);
+h2.textContent = "Hi this is html"
+console.log(h2);
+
+customDiv.innerHTML = h2;
+
+
+
